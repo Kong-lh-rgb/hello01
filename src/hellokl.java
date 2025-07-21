71b81e84a94ad04d2d6e315e8a6ff71ff41b87fe
Your Name <you@example.com>
2025-07-21 17:39:23 +0800

    更新项目内容

--- src/hellokl.java
+++ src/hellokl.java
@@ -1,5 +1,13 @@
 public class hellokl {
     public static void main(String[] args) {
+        String a="姓名：孔令航";
+        int b=18;
+        char gender='男';
+        double height=184;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(gender);
+        System.out.println(height);
         System.out.println("hello klh");
     }
 }
