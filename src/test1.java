58123d8b65ca6f3d0349d2e3a9c1e68f6eefb7c4
Your Name <you@example.com>
2025-07-21 17:53:55 +0800

    键盘录入的学习

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,13 @@
+//Scanner导包
+import java.util.Scanner;
 public class test1 {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //接收数据
+        System.out.println("请输入整数");
+        int i=sc.nextInt();
         String name="送你回家";
         System.out.println(name);
+        System.out.println(i);
     }
 }
