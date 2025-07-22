439209bec2d1d573b55b2e4b432ea566e45820e1
Your Name <you@example.com>
2025-07-22 13:48:27 +0800

    三元运算符的使用

--- day-03/src/com/lxny/yunusanfu.java
+++ day-03/src/com/lxny/yunusanfu.java
@@ -1,5 +1,7 @@
 package com.lxny;
 
+import java.util.Scanner;
+
 public class yunusanfu {
     public static void main(String[] args) {
         System.out.println(3 + 2);
@@ -7,5 +9,11 @@ public class yunusanfu {
         System.out.println(5 / 2);
         System.out.println(5 % 2);
 
+        //三元运算符
+        Scanner sc=new Scanner(System.in);
+        int number1=sc.nextInt();
+        int number2=sc.nextInt();
+        int max=number1>number2?number1:number2;
+        System.out.println(max);
     }
 }
