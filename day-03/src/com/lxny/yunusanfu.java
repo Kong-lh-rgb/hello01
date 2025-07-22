107a59f2d8a196d250ad1759ebd50258c4746f83
Your Name <you@example.com>
2025-07-22 13:39:13 +0800

    运算符的使用

---
+++ day-03/src/com/lxny/yunusanfu.java
@@ -0,0 +1,11 @@
+package com.lxny;
+
+public class yunusanfu {
+    public static void main(String[] args) {
+        System.out.println(3 + 2);
+        System.out.println(5 * 7);
+        System.out.println(5 / 2);
+        System.out.println(5 % 2);
+
+    }
+}
